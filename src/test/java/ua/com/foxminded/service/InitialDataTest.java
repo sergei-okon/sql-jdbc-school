f1cb63b7af653052c85068908668cfa3deceecb4
sergei-okon <sergei.okon@gmail.com>
2022-01-05 21:39:47 +0200

    Task 7 fixed bugs

---
+++ src/test/java/ua/com/foxminded/service/InitialDataTest.java
@@ -0,0 +1,25 @@
+package ua.com.foxminded.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InitialDataTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addGroupsToDataBase() {
+    }
+
+    @Test
+    void addStudentsToDataBase() {
+    }
+
+    @Test
+    void addCoursesToDataBase() {
+    }
+}
